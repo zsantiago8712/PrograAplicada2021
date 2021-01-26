9a8ca10a2122c645e759fae0828998d2dad8453e
Santiago Zamora <zsantiago8788@icloud.com>
2021-01-26 10:56:58 -0600

    Clase 26-01-21 Apuntadores

---
+++ Clase-26-01-21/apuntadores.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int suma(int *x, int *y);
+
+int main(void)
+{
+    int x = 5;
+    int y = 4;
+
+    suma(&x, &y);
+    printf("%d %d\n", x, y);
+
+}
+
+int suma(int *x, int *y)
+{
+    (*x)++;     /* ----> para poder modificar el valor debes poner (*variable) significa que trae el valor 
+                y despues lo modifica en el caso le suma 1*/
+    (*y)++;
+    
+    
+            
+
+}
